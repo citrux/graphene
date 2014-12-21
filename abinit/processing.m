8300d83ec95f9113ee6b1c4db0aee9f363d653c7
Vova Abdrakhmanov <369565@gmail.com>
2014-12-21 15:27:09 +0300

    удачно разобрал зоны и начал писать отчёт

--- abinit/processing.m
+++ abinit/processing.m
@@ -14,24 +14,30 @@ import[lst_] := Module[{data},
         {i, lst}];
     data]
 
-perm[data_, p_] := Module[{a},
+perm[data_, p_, nband_] := Module[{a},
     a = data;
     Do[
         Do[
             If[ Abs[a[[j]] - p[[i]]] < Abs[a[[i]] - p[[i]]],
                 {a[[i]], a[[j]]}={a[[j]], a[[i]]}],
             {j, i, Length[p]}],
-        {i, Length[p]}];
+        {i, nband}];
     a]
 
 bands[data_] := Module[{result},
 	result = data[[;;2]];
     Do[
-        AppendTo[result,perm[data[[i]], 2 result[[-1]] - result[[-2]]]],
+        AppendTo[result,perm[data[[i]], 2 result[[-1]] - result[[-2]], Length[result[[-1]]]]],
         {i, 3, Length[data]}];
     Transpose[result]]
 
 data = import[Import["grapheneo_DS2_EIG", "List"]];
+data1 = Map[Reverse, data[[;;51]]];
+data2 = Map[Reverse, data[[52;;]]];
+bands1 = Map[Reverse, bands[data1]];
+bands2 = bands[data2];
+result = {};
+Do[AppendTo[result,Join[bands1[[i]], bands2[[i]]]], {i, {3, 19, 20, 21, 22}}];
 Export["bands.png",
-        ListLinePlot[bands[data], DataRange -> {0, 30}, PlotRange -> All],
+        ListLinePlot[result, PlotRange -> All],
         ImageSize->{800,600}];
